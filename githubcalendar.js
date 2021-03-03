f5dd54197c1b3e7437a72e3a7f049203a2cd0712
Zfour <499984532@qq.com>
2021-03-03 20:05:14 +0800

    Update githubcalendar.js

--- githubcalendar.js
+++ githubcalendar.js
@@ -250,8 +250,8 @@ let loading_git = (color) => {
     loading = '<div id="github_loading" style="width:10%;height:100%;margin:0 auto;display: block"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 50 50" style="enable-background:new 0 0 50 50" xml:space="preserve"><path fill="' + color + '" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z" transform="rotate(275.098 25 25)"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform></path></svg></div>';
     return loading
 };
-window.onload = function () {
-    let git_user = 'Zfour';
+
+(function(){let git_user = 'Zfour';
     let github_container = document.getElementById('github_container');
     let github_loading = document.getElementById('github_loading');
     let git_purple = ['#ebedf0', '#fdcdec', '#fc9bd9', '#fa6ac5', '#f838b2', '#f5089f', '#c4067e', '#92055e', '#540336', '#48022f', '#30021f',];
@@ -259,5 +259,6 @@ window.onload = function () {
     let git_blue = ['#ebedf0', '#f1f8ff', '#dbedff', '#c8e1ff', '#79b8ff', '#2188ff', '#0366d6', '#005cc5', '#044289', '#032f62', '#05264c',];
     let git_color = git_purple;
     append_div_gitcalendar(github_container, loading_git(git_color[4]));
-    github_canlendar(git_user, git_color)
-}
+    github_canlendar(git_user, git_color)})()
+    
+    
