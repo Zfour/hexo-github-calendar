fdb3d640311e4121912d1a608686fa61bc6d2b6c
Zfour <499984532@qq.com>
2021-03-04 17:41:13 +0800

    Update github_calendar.js

--- plug_in/github_calendar.js
+++ plug_in/github_calendar.js
@@ -1,5 +1,5 @@
 /**
- * Butterfly Github Canlendar
+ * Hexo Github Canlendar
  * Ice Kano
  */
 
@@ -16,16 +16,21 @@ hexo.extend.helper.register('github_calendar_js_cdn', function(link){
 hexo.extend.filter.register('after_generate',function(){
   logger.info('已导入小冰插件：Butterfly Github Canlendar')
   var github_calendar = hexo.theme.config.githubcalendar.enable;
+  var calendar_js = hexo.theme.config.githubcalendar.calendar_js;
+  var github_color = hexo.theme.config.githubcalendar.color;
+  var layout_id = hexo.theme.config.githubcalendar.layout_id;
+  var githubcalendar_html = hexo.theme.config.githubcalendar.githubcalendar_html;
   var github_user = hexo.theme.config.githubcalendar.user;
   var github_api = hexo.theme.config.githubcalendar.api;
-  var github_color = hexo.theme.config.githubcalendar.color;
+  var pc_minheight = hexo.theme.config.githubcalendar.pc_minheight;
+  var mobile_minheight = hexo.theme.config.githubcalendar.mobile_minheight;
   var user_info_js = '<script>' + 'let git_githubapiurl ="'+ github_api + '?' + github_user +'";'+
       'let git_color ='+ github_color +';'+
       'let git_user ="'+ github_user +'";'+
-      'let parent_div_git = document.getElementById("recent-posts");'+
-      'let git_div_html = '+ "'"+'<div class="recent-post-item" style="width:100%;height:auto;padding:10px"><div id="github_container"></div></div>'+ "'"+';'+
-      'parent_div_git.innerHTML=git_div_html+parent_div_git.innerHTML'+
-      '</script>'
+      'let parent_div_git = document.getElementById('+'"'+ layout_id +'"'+');'+
+      'let git_div_html = '+ "'"+ githubcalendar_html + "'"+';'+
+      'parent_div_git.innerHTML=git_div_html+parent_div_git.innerHTML'+';'+
+      '</script><style>'+'#github_container{min-height:'+pc_minheight+'}'+'@media screen and (max-width:650px) {#github_container{background-image:;min-height:'+mobile_minheight+'}}'+'</style>'
 
   hexo.extend.injector.register('body_end',user_info_js, 'default');
 
@@ -34,7 +39,7 @@ hexo.extend.filter.register('after_generate',function(){
   //查询已定义函数并命名
   const github_calendar_js_cdn = hexo.extend.helper.get('github_calendar_js_cdn').bind(hexo);
   // inject插入 Github Canlendar js依赖
-  hexo.extend.injector.register('body_end',github_calendar_js_cdn('https://cdn.jsdelivr.net/gh/Zfour/hexo-github-calendar/hexo_githubcalendar.js'), 'default');
+  hexo.extend.injector.register('body_end',github_calendar_js_cdn(calendar_js), 'default');
 })
 
 
