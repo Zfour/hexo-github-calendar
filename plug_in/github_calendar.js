18517856cb324827448829e9f4a7a83b58edcf53
Lete乐特 <48512251+lete114@users.noreply.github.com>
2021-03-05 06:33:57 +0800

    Update github_calendar.js

--- plug_in/github_calendar.js
+++ plug_in/github_calendar.js
@@ -1,17 +1,13 @@
 /**
  * Hexo Github Canlendar
  * Ice Kano
+ * Modify: Lete乐特
  */
 
 'use strict'
 
 //导入 hexo logger
 const logger = require('hexo-log')()
-
-//定义函数
-hexo.extend.helper.register('github_calendar_js_cdn', function(link){
-  return   '<script data-pjax src="'+ link +'"></script>'
-});
 
 hexo.extend.filter.register('after_generate',function(){
   var github_calendar = hexo.theme.config.githubcalendar.enable;
@@ -26,30 +22,25 @@ hexo.extend.filter.register('after_generate',function(){
   var github_api = hexo.theme.config.githubcalendar.api;
   var pc_minheight = hexo.theme.config.githubcalendar.pc_minheight;
   var mobile_minheight = hexo.theme.config.githubcalendar.mobile_minheight;
-  var user_info_js = '<script data-pjax>' + 'var git_githubapiurl ="'+ github_api + '?' + github_user +'";'+
-      'var git_color ='+ github_color +';'+
-      'var git_user ="'+ github_user +'";'+
-      'var parent_div_git = document.getElementById('+'"'+ layout_id +'"'+');'+
-      'var git_div_html = '+ "'"+ githubcalendar_html + "'"+';'+
-      'if(parent_div_git){parent_div_git.innerHTML=git_div_html+parent_div_git.innerHTML}'+';'+
-      '</script><style>'+'#github_container{min-height:'+pc_minheight+'}'+'@media screen and (max-width:650px) {#github_container{background-image:;min-height:'+mobile_minheight+'}}'+'</style>'
-
-  hexo.extend.injector.register('body_end',user_info_js, 'default');
-
-
-
-  //查询已定义函数并命名
-  const github_calendar_js_cdn = hexo.extend.helper.get('github_calendar_js_cdn').bind(hexo);
-  // inject插入 Github Canlendar js依赖
-  hexo.extend.injector.register('body_end',github_calendar_js_cdn(calendar_js),'default');
+  var user_info_js = `
+  <script data-pjax src="${calendar_js}"></script>
+  <script data-pjax>
+        function GithubCalendarConfig(){
+            var git_githubapiurl ="${github_api}?${github_user}";
+            var git_color =${github_color};
+            var git_user ="${github_user}";
+            var parent_div_git = document.getElementById("${layout_id}");
+            var git_div_html = '${githubcalendar_html}';
+            if(parent_div_git){
+                // parent_div_git.innerHTML=git_div_html+parent_div_git.innerHTML // 无报错，但不影响使用(支持pjax跳转)
+                parent_div_git.insertAdjacentHTML("afterbegin",git_div_html) // 有报错，但不影响使用(支持pjax跳转)
+            };
+            GithubCalendar(git_githubapiurl,git_color,git_user)
+        }
+        if(document.getElementById("${layout_id}")){
+            GithubCalendarConfig()
+        }
+    </script>
+    <style>#github_container{min-height:${pc_minheight}}@media screen and (max-width:650px) {#github_container{background-image:;min-height:${mobile_minheight}}}</style>`
+    hexo.extend.injector.register('body_end',user_info_js, "default");
 }})
-
-
-
-
-
-
-
-
-
-
